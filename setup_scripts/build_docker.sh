ec93613da374dc9c94f44922128b6da975074431
Daniil Lisus <lisus.daniil@gmail.com>
2023-03-18 12:45:01 -0400

    Docker build unique images

--- setup_scripts/build_docker.sh
+++ setup_scripts/build_docker.sh
@@ -1,6 +1,6 @@
 # Assumes that ROOTDIR is set and pointing to radar_topometric_localization root directory
 cd $ROOTDIR
-docker build --rm -t radar_loc \
+docker build --rm -t radar_loc_$(whoami) \
   --build-arg USERID=$(id -u) \
   --build-arg GROUPID=$(id -g) \
   --build-arg USERNAME=$(whoami) \
