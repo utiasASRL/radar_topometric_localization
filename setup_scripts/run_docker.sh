94c07c1d32650c32c54dbd3eb35c5b0fa02a6daf
Daniil Lisus <lisus.daniil@gmail.com>
2023-03-18 13:14:47 -0400

    Docker runfile bugfix

--- setup_scripts/run_docker.sh
+++ setup_scripts/run_docker.sh
@@ -1,12 +1,12 @@
 # Assumes that ROOTDIR is set and pointing to radar_topometric_localization root directory
-if [ $(docker inspect -f '{{.State.Running}}' radar_loc) = "true" ]
+if [ $(docker inspect -f '{{.State.Running}}' radar_loc_$(whoami)) = "true" ]
 then
 	echo 'Container already running, joining it now.'
-	docker exec -it radar_loc bash
+	docker exec -it radar_loc_$(whoami) bash
     cd $ROOTDIR
 else
 	echo 'New container run initialized.'
-	docker run -it --rm --name radar_loc \
+	docker run -it --rm --name radar_loc_$(whoami) \
 	--privileged \
 	--network=host \
 	--gpus all \
@@ -16,5 +16,5 @@ else
 	-v ${HOME}/.Xauthority:${HOME}/.Xauthority:rw \
 	-v ${HOME}:${HOME}:rw \
 	-v $ROOTDIR:$ROOTDIR:rw \
-	-w $ROOTDIR radar_loc
+	-w $ROOTDIR radar_loc_$(whoami)
 fi
