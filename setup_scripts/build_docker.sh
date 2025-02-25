713d51a7c2ceebfc5e19629f7d84b85baa4b8e74
Daniil <lisus.daniil@gmail.com>
2025-02-24 20:37:08 -0500

    (QoL) Speed up Docker re-building

--- setup_scripts/build_docker.sh
+++ setup_scripts/build_docker.sh
@@ -1,6 +1,6 @@
 # Assumes that ROOTDIR is set and pointing to radar_topometric_localization root directory
 cd $ROOTDIR
-docker build --no-cache --rm -t radar_loc_$(whoami) \
+docker build --rm -t radar_loc_$(whoami) \
   --build-arg USERID=$(id -u) \
   --build-arg GROUPID=$(id -g) \
   --build-arg USERNAME=$(whoami) \
