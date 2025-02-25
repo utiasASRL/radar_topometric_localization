0229327d509da593729e54eb8ae85c3ec465ce54
Daniil <lisus.daniil@gmail.com>
2025-02-24 20:35:55 -0500

    (QoL) cap number of processes used for building packages. Helps prevent computer freezing during build.

--- setup_scripts/build_vtr3.sh
+++ setup_scripts/build_vtr3.sh
@@ -1,6 +1,6 @@
 # Assumes that ROOTDIR is set and pointing to radar_topometric_localization root directory
 source /opt/ros/humble/setup.bash  # source the ROS environment
 cd $ROOTDIR/external/vtr3/main # Go to where vtr3 is located
-VTR_PIPELINE=LIDAR colcon build --symlink-install --packages-up-to vtr_lidar vtr_radar vtr_radar_lidar --allow-overriding vtr_common vtr_lidar vtr_logging vtr_radar vtr_radar_lidar --cmake-args -DCMAKE_BUILD_TYPE=Release
+MAKEFLAGS="-j$(($(nproc --all) / 4 + 1))" VTR_PIPELINE=LIDAR colcon build --symlink-install --packages-up-to vtr_lidar vtr_radar vtr_radar_lidar --allow-overriding vtr_common vtr_lidar vtr_logging vtr_radar vtr_radar_lidar --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 cd $ROOTDIR
--- setup_scripts/build_vtr_testing_radar.sh
+++ setup_scripts/build_vtr_testing_radar.sh
@@ -4,6 +4,6 @@ export VTRSRC=$ROOTDIR/external/vtr3
 source /opt/ros/humble/setup.bash
 source ${VTRSRC}/main/install/setup.bash # source the vtr3 environment
 cd $ROOTDIR/external/boreas_vtr_wrapper # go to where vtr_boreas_vtr_wrappertesting_radar is located
-colcon build --packages-select vtr_testing_aeva --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
+MAKEFLAGS="-j$(($(nproc --all) / 4 + 1))" colcon build --packages-select vtr_testing_aeva --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 cd $ROOTDIR
